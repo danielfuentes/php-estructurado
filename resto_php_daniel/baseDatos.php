f9586d6f57c34628611849962ab5da5d79f3bdf5
DigitalHouse <cedavilu@gmail.com>
2019-09-06 19:07:05 -0300

    Código PHP para trabajar con Array

---
+++ resto_php_daniel/baseDatos.php
@@ -0,0 +1,23 @@
+<?php
+//Amigas y amigos. Aquí les creo un array asociativo, en el cual voy a simular que son los productos que estan registrado en una base de datos, mas adelante esto lo vamos a trabajar pero un archivo bajo el formato JSON
+$articulos = [
+	0=>[
+		"imagen"=>"img-pdto-1.jpg",
+		"titulo"=>"Lorem ipsum amet",
+		"descripcion"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex."			
+	],
+	1=>[
+		"imagen"=>"img-pdto-2.jpg",
+		"titulo"=>"Lorem ipsum amet",
+		"descripcion"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex."			
+	],
+	2=>[
+		"imagen"=>"img-pdto-3.jpg",
+		"titulo"=>"Lorem ipsum amet",
+		"descripcion"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex."			
+	]
+
+];
+
+
+?>
